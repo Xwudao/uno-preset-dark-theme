718f4fa20facf02ea3c016cb1a8d31115d8adcf0
kuan <apkapb@gmail.com>
2022-08-14 19:55:18 +0800

    init

---
+++ README.md
@@ -0,0 +1,44 @@
+## install
+
+```bash
+pnpm i uno-preset-dark-theme -D
+```
+
+## config
+
+```ts
+export default defineConfig({
+  rules: [],
+  theme: {
+    dark:{
+		colors: {
+			primary: '#e67e22',
+			black: '#17171a',
+		},
+    },
+    light:{
+		colors: {
+			primary: '#e67e22',
+			black: '#17171a',
+		},
+    }
+  },
+  presets: [
+    presetTheme({
+    	element: 'body'
+    }),
+    // other preset, such as PresetWind
+    // but, this preset should be used with other presets
+  ],
+});
+
+```
+
+
+
+### Copyright
+
+this code copied from [unocss/unocss](https://github.com/unocss/unocss/blob/7ac234c27a8d7d087523f0be313b154a1fd4afe7/packages/preset-theme/src/index.ts)
+
+
+And, I modified something...
---
+++ src/index.ts
@@ -0,0 +1,79 @@
+import type { Preset } from "@unocss/core";
+import { mergeDeep } from "@unocss/core";
+import type { Theme } from "@unocss/preset-mini";
+export interface PresetTheme {
+  // theme object
+  theme: Record<"dark" | "light", Theme>;
+  // default: --un-preset-theme
+  prefix?: string;
+  // generated style attach to which element
+  // eg: body{--var-name:'#fff'}
+  //     body[data-theme="dark"]{--var-name:'#000'}
+  element?: string;
+}
+
+export const presetTheme = (options: PresetTheme): Preset<Theme> => {
+  const { prefix = "--un-preset-theme", element = "body" } = options;
+  const { dark, light } = options.theme;
+  // const themeValues = new Map<
+  //   string,
+  //   {
+  //     light?: string;
+  //     dark?: string;
+  //   }
+  // >();
+  // const varsRE = new RegExp(`var\\((${prefix}.*)\\)`);
+
+  const lightPreflightCss: string[] = [];
+  const darkPreflightCss: string[] = [];
+
+  const getTheme = (theme: any, keys: string[]) => {
+    for (const key of keys) {
+      theme = theme[key];
+      if (theme === undefined) return;
+    }
+    return theme;
+  };
+
+  const recursiveTheme = (
+    theme: Record<string, any>,
+    preKeys: string[] = []
+  ) => {
+    Object.keys(theme).forEach((key) => {
+      const val = Reflect.get(theme, key);
+      const nextKeys = preKeys.concat(key);
+
+      if (typeof val !== "object" && !Array.isArray(val)) {
+        const varName = `${prefix}-${nextKeys.join("-")}`;
+
+        lightPreflightCss.push(`${varName}: ${getTheme(light, nextKeys)}`);
+        darkPreflightCss.push(`${varName}: ${getTheme(dark, nextKeys)}`);
+
+        theme[key] = `var(${varName})`;
+      } else {
+        recursiveTheme(val, nextKeys);
+      }
+    });
+    return theme;
+  };
+
+  const theme = recursiveTheme(mergeDeep(dark, light));
+  console.log(theme);
+
+  return {
+    name: "@unocss/preset-theme",
+    theme: { ...theme },
+    preflights: [
+      {
+        layer: "theme",
+        async getCSS() {
+          return `${element}{${lightPreflightCss.join(";")}}\n
+          ${element}[data-theme='dark']{${darkPreflightCss.join(";")}}`;
+        },
+      },
+    ],
+    prefix: options.prefix,
+  };
+};
+
+export default presetTheme;
